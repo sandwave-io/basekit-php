e78d4ce5b516f1e6f50082e15c52b3e0626b1954
Stephan van der Horn <stephan@sandwave.io>
2025-01-03 13:20:13 +0000

    Modernize code and make Capabilities nullable

--- src/Domain/Capabilities.php
+++ src/Domain/Capabilities.php
@@ -4,122 +4,30 @@ namespace SandwaveIo\BaseKit\Domain;
 
 final class Capabilities implements DomainObjectInterface
 {
-    public string $liveSites;
-
-    public string $allowUsers;
-
-    public string $cdnEnabled;
-
-    public string $cssEditing;
-
-    public string $themeLevel;
-
-    public string $freeDomains;
-
-    public string $htmlEditing;
-
-    public string $pagesLimited;
-
-    public string $storageLimit;
-
-    public string $templateTier;
-
-    public string $domainMapping;
-
-    public string $googleAnalytics;
-
-    public string $ecommerceAllowed;
-
-    public string $googleAdWordVoucher;
-
-    public string $allowExternalRedirects;
-
-    public string $allowTemplateSave;
-
-    public string $mailboxes;
-
-    public string $mobile;
-
-    public ?string $siteLock;
-
-    public string $mobileSites;
-
-    public string $mobilePublishing;
-
-    public string $restrictPagesOnPublish;
-
-    /**
-     * Capabilities constructor.
-     *
-     * @param string      $liveSites
-     * @param string      $allowUsers
-     * @param string      $cdnEnabled
-     * @param string      $cssEditing
-     * @param string      $themeLevel
-     * @param string      $freeDomains
-     * @param string      $htmlEditing
-     * @param string      $pagesLimited
-     * @param string      $storageLimit
-     * @param string      $templateTier
-     * @param string      $domainMapping
-     * @param string      $googleAnalytics
-     * @param string      $ecommerceAllowed
-     * @param string      $googleAdWordVoucher
-     * @param string      $allowExternalRedirects
-     * @param string      $allowTemplateSave
-     * @param string      $mailboxes
-     * @param string      $mobile
-     * @param string|null $siteLock
-     * @param string      $mobileSites
-     * @param string      $mobilePublishing
-     * @param string      $restrictPagesOnPublish
-     */
     public function __construct(
-        string $liveSites,
-        string $allowUsers,
-        string $cdnEnabled,
-        string $cssEditing,
-        string $themeLevel,
-        string $freeDomains,
-        string $htmlEditing,
-        string $pagesLimited,
-        string $storageLimit,
-        string $templateTier,
-        string $domainMapping,
-        string $googleAnalytics,
-        string $ecommerceAllowed,
-        string $googleAdWordVoucher,
-        string $allowExternalRedirects,
-        string $allowTemplateSave,
-        string $mailboxes,
-        string $mobile,
-        ?string $siteLock,
-        string $mobileSites,
-        string $mobilePublishing,
-        string $restrictPagesOnPublish
+        public ?string $liveSites = null,
+        public ?string $allowUsers = null,
+        public ?string $cdnEnabled = null,
+        public ?string $cssEditing = null,
+        public ?string $themeLevel = null,
+        public ?string $freeDomains = null,
+        public ?string $htmlEditing = null,
+        public ?string $pagesLimited = null,
+        public ?string $storageLimit = null,
+        public ?string $templateTier = null,
+        public ?string $domainMapping = null,
+        public ?string $googleAnalytics = null,
+        public ?string $ecommerceAllowed = null,
+        public ?string $googleAdWordVoucher = null,
+        public ?string $allowExternalRedirects = null,
+        public ?string $allowTemplateSave = null,
+        public ?string $mailboxes = null,
+        public ?string $mobile = null,
+        public ?string $siteLock = null,
+        public ?string $mobileSites = null,
+        public ?string $mobilePublishing = null,
+        public ?string $restrictPagesOnPublish = null,
     ) {
-        $this->liveSites = $liveSites;
-        $this->allowUsers = $allowUsers;
-        $this->cdnEnabled = $cdnEnabled;
-        $this->cssEditing = $cssEditing;
-        $this->themeLevel = $themeLevel;
-        $this->freeDomains = $freeDomains;
-        $this->htmlEditing = $htmlEditing;
-        $this->pagesLimited = $pagesLimited;
-        $this->storageLimit = $storageLimit;
-        $this->templateTier = $templateTier;
-        $this->domainMapping = $domainMapping;
-        $this->googleAnalytics = $googleAnalytics;
-        $this->ecommerceAllowed = $ecommerceAllowed;
-        $this->googleAdWordVoucher = $googleAdWordVoucher;
-        $this->allowExternalRedirects = $allowExternalRedirects;
-        $this->allowTemplateSave = $allowTemplateSave;
-        $this->mailboxes = $mailboxes;
-        $this->mobile = $mobile;
-        $this->siteLock = $siteLock;
-        $this->mobileSites = $mobileSites;
-        $this->mobilePublishing = $mobilePublishing;
-        $this->restrictPagesOnPublish = $restrictPagesOnPublish;
     }
 
     /**
@@ -130,28 +38,28 @@ final class Capabilities implements DomainObjectInterface
     public static function fromArray(array $json): Capabilities
     {
         return new Capabilities(
-            $json['liveSites'],
-            $json['allowUsers'],
-            $json['cdnEnabled'],
-            $json['cssEditing'],
-            $json['themeLevel'],
-            $json['freeDomains'],
-            $json['htmlEditing'],
-            $json['pagesLimited'],
-            $json['storageLimit'],
-            $json['templateTier'],
-            $json['domainMapping'],
-            $json['googleAnalytics'],
-            $json['ecommerceAllowed'],
-            $json['googleAdWordVoucher'],
-            $json['allowExternalRedirects'],
-            $json['allowTemplateSave'],
-            $json['mailboxes'],
-            $json['mobile'],
-            $json['siteLock'] ?? null,
-            $json['mobileSites'],
-            $json['mobilePublishing'],
-            $json['restrictPagesOnPublish'],
+            liveSites: $json['liveSites'],
+            allowUsers: $json['allowUsers'],
+            cdnEnabled: $json['cdnEnabled'],
+            cssEditing: $json['cssEditing'],
+            themeLevel: $json['themeLevel'],
+            freeDomains: $json['freeDomains'],
+            htmlEditing: $json['htmlEditing'],
+            pagesLimited: $json['pagesLimited'],
+            storageLimit: $json['storageLimit'],
+            templateTier: $json['templateTier'],
+            domainMapping: $json['domainMapping'],
+            googleAnalytics: $json['googleAnalytics'],
+            ecommerceAllowed: $json['ecommerceAllowed'],
+            googleAdWordVoucher: $json['googleAdWordVoucher'],
+            allowExternalRedirects: $json['allowExternalRedirects'],
+            allowTemplateSave: $json['allowTemplateSave'],
+            mailboxes: $json['mailboxes'],
+            mobile: $json['mobile'],
+            siteLock: $json['siteLock'] ?? null,
+            mobileSites: $json['mobileSites'],
+            mobilePublishing: $json['mobilePublishing'],
+            restrictPagesOnPublish: $json['restrictPagesOnPublish'],
         );
     }
 
