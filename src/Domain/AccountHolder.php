8503db3216e2777f206dc10eaf9bedba253f4e41
Stephan van der Horn <stephan@sandwave.io>
2025-01-03 10:41:12 +0100

    Modernize code and make Capabilities nullable

--- src/Domain/AccountHolder.php
+++ src/Domain/AccountHolder.php
@@ -4,149 +4,41 @@ namespace SandwaveIo\BaseKit\Domain;
 
 final class AccountHolder implements DomainObjectInterface
 {
-    public int $ref;
-
-    public string $firstName;
-
-    public string $lastName;
-
-    public string $username;
-
-    public string $email;
-
-    public int $suspended;
-
-    public bool $beta;
-
-    public string $languageCode;
-
-    public ?string $phoneNumber;
-
-    public ?string $address1;
-
-    public ?string $address2;
-
-    public ?string $city;
-
-    public ?string $postcode;
-
-    public ?string $country;
-
-    public int $newsletter;
-
-    public int $currencyRef;
-
-    public ?string $state;
-
-    public Capabilities $capabilities;
-
-    public ?int  $accountPaymentMethodRef;
-
-    public ?string $cpfNumber;
-
-    public int $cpfCompany;
-
-    public bool $deleted;
-
-    public int $storageBytesUsed;
-
-    /** @var array<mixed>|null */
-    public ?array $created;
-
-    /** @var array<string, string|int>|null */
-    public ?array $lastLogin;
-
-    public string $accountStatus;
-
-    public ?int $resellerRef;
-
     /**
-     * AccountHolder constructor.
-     *
-     * @param int                            $ref
-     * @param string                         $firstName
-     * @param string                         $lastName
-     * @param string                         $username
-     * @param string                         $email
-     * @param int                            $suspended
-     * @param bool                           $beta
-     * @param string                         $languageCode
-     * @param string|null                    $phoneNumber
-     * @param string|null                    $address1
-     * @param string|null                    $address2
-     * @param string|null                    $city
-     * @param string|null                    $postcode
-     * @param string|null                    $country
-     * @param int                            $newsletter
-     * @param int                            $currencyRef
-     * @param string|null                    $state
-     * @param Capabilities                   $capabilities
-     * @param int|null                       $accountPaymentMethodRef
-     * @param string|null                    $cpfNumber
-     * @param int                            $cpfCompany
-     * @param bool                           $deleted
-     * @param int                            $storageBytesUsed
      * @param array<mixed>|null              $created
      * @param array<string, string|int>|null $lastLogin
-     * @param string                         $accountStatus
-     * @param int|null                       $resellerRef
      */
     public function __construct(
-        int $ref,
-        string $firstName,
-        string $lastName,
-        string $username,
-        string $email,
-        int $suspended,
-        bool $beta,
-        string $languageCode,
-        ?string $phoneNumber,
-        ?string $address1,
-        ?string $address2,
-        ?string $city,
-        ?string $postcode,
-        ?string $country,
-        int $newsletter,
-        int $currencyRef,
-        ?string $state,
-        Capabilities $capabilities,
-        ?int $accountPaymentMethodRef,
-        ?string $cpfNumber,
-        int $cpfCompany,
-        bool $deleted,
-        int $storageBytesUsed,
-        ?array $created,
-        ?array $lastLogin,
-        string $accountStatus,
-        ?int $resellerRef,
+        public int $ref,
+        public int $brandRef,
+        public string $firstName,
+        public string $lastName,
+        public string $username,
+        public string $email,
+        public int $suspended,
+        public bool $beta,
+        public string $languageCode,
+        public ?string $phoneNumber,
+        public ?string $address1,
+        public ?string $address2,
+        public ?string $city,
+        public ?string $postcode,
+        public ?string $country,
+        public int $newsletter,
+        public int $currencyRef,
+        public ?string $state,
+        public Capabilities $capabilities,
+        public ?int $accountPaymentMethodRef,
+        public ?string $cpfNumber,
+        public int $cpfCompany,
+        public string $company,
+        public bool $deleted,
+        public int $storageBytesUsed,
+        public ?array $created,
+        public ?array $lastLogin,
+        public string $accountStatus,
+        public ?int $resellerRef,
     ) {
-        $this->ref = $ref;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->username = $username;
-        $this->email = $email;
-        $this->suspended = $suspended;
-        $this->beta = $beta;
-        $this->languageCode = $languageCode;
-        $this->phoneNumber = $phoneNumber;
-        $this->address1 = $address1;
-        $this->address2 = $address2;
-        $this->city = $city;
-        $this->postcode = $postcode;
-        $this->country = $country;
-        $this->newsletter = $newsletter;
-        $this->currencyRef = $currencyRef;
-        $this->state = $state;
-        $this->capabilities = $capabilities;
-        $this->accountPaymentMethodRef = $accountPaymentMethodRef;
-        $this->cpfNumber = $cpfNumber;
-        $this->cpfCompany = $cpfCompany;
-        $this->deleted = $deleted;
-        $this->storageBytesUsed = $storageBytesUsed;
-        $this->created = $created;
-        $this->lastLogin = $lastLogin;
-        $this->accountStatus = $accountStatus;
-        $this->resellerRef = $resellerRef;
     }
 
     /**
@@ -157,33 +49,35 @@ final class AccountHolder implements DomainObjectInterface
     public static function fromArray(array $json): AccountHolder
     {
         return new AccountHolder(
-            $json['ref'],
-            $json['firstName'],
-            $json['lastName'],
-            $json['username'],
-            $json['email'],
-            $json['suspended'],
-            $json['beta'],
-            $json['languageCode'],
-            $json['phoneNumber'] ?? null,
-            $json['address1'] ??  null,
-            $json['address2'] ?? null,
-            $json['city']?? null,
-            $json['postcode'] ?? null,
-            $json['country'] ?? null,
-            $json['newsletter'],
-            $json['currencyRef'],
-            $json['state'] ?? null,
-            Capabilities::fromArray($json['capabilities']),
-            $json['accountPaymentMethodRef'] ?? null,
-            $json['cpfNumber'] ?? null,
-            $json['cpfCompany'],
-            $json['deleted'],
-            (int) $json['storageBytesUsed'],
-            $json['created'] ?? null,
-            $json['lastLogin'] ?? null,
-            $json['accountStatus'],
-            $json['resellerRef'] ?? null,
+            ref: $json['ref'],
+            brandRef: $json['brandRef'],
+            firstName: $json['firstName'],
+            lastName: $json['lastName'],
+            username: $json['username'],
+            email: $json['email'],
+            suspended: $json['suspended'],
+            beta: $json['beta'],
+            languageCode: $json['languageCode'],
+            phoneNumber: $json['phoneNumber'] ?? null,
+            address1: $json['address1'] ??  null,
+            address2: $json['address2'] ?? null,
+            city: $json['city']?? null,
+            postcode: $json['postcode'] ?? null,
+            country: $json['country'] ?? null,
+            newsletter: $json['newsletter'],
+            currencyRef: $json['currencyRef'],
+            state: $json['state'] ?? null,
+            capabilities: Capabilities::fromArray($json['capabilities']),
+            accountPaymentMethodRef: $json['accountPaymentMethodRef'] ?? null,
+            cpfNumber: $json['cpfNumber'] ?? null,
+            cpfCompany: $json['cpfCompany'],
+            company: $json['company'],
+            deleted: $json['deleted'],
+            storageBytesUsed: (int) $json['storageBytesUsed'],
+            created: $json['created'] ?? null,
+            lastLogin: $json['lastLogin'] ?? null,
+            accountStatus: $json['accountStatus'],
+            resellerRef: $json['resellerRef'] ?? null,
         );
     }
 
@@ -194,6 +88,7 @@ final class AccountHolder implements DomainObjectInterface
     {
         return [
             'ref' => $this->ref,
+            'brandRef' => $this->brandRef,
             'firstName' => $this->firstName,
             'lastName' => $this->lastName,
             'username' => $this->username,
--- src/Domain/Site.php
+++ src/Domain/Site.php
@@ -4,66 +4,25 @@ namespace SandwaveIo\BaseKit\Domain;
 
 final class Site implements DomainObjectInterface
 {
-    public int $ref;
-
-    /** @var Domain[] */
-    public array $domains;
-
-    public ?int $contentMapSite;
-
-    public ?int $template;
-
-    public Domain $primaryDomain;
-
-    /** @var array<string, string|int>|null */
-    public ?array $lastPublish;
-
-    public int $brandRef;
-
-    public int $version;
-
-    public bool $enabled;
-
-    public ?bool $privateWidgets;
-
-    public ?int $mobileSiteRef;
-
-    public bool $mobile;
-
-    public ?int $profileRef;
-
     /**
-     * Site constructor.
-     *
-     * @param int                        $ref
      * @param Domain[]                   $domains
-     * @param int|null                   $contentMapSite
-     * @param int|null                   $template
-     * @param Domain                     $primaryDomain
      * @param ?array<string, string|int> $lastPublish
-     * @param int                        $brandRef
-     * @param int                        $version
-     * @param bool                       $enabled
-     * @param bool|null                  $privateWidgets
-     * @param int|null                   $mobileSiteRef
-     * @param bool                       $mobile
-     * @param int|null                   $profileRef
      */
-    public function __construct(int $ref, array $domains, ?int $contentMapSite, ?int $template, Domain $primaryDomain, ?array $lastPublish, int $brandRef, int $version, bool $enabled, ?bool $privateWidgets, ?int $mobileSiteRef, bool $mobile, ?int $profileRef)
-    {
-        $this->ref = $ref;
-        $this->domains = $domains;
-        $this->contentMapSite = $contentMapSite;
-        $this->template = $template;
-        $this->primaryDomain = $primaryDomain;
-        $this->lastPublish = $lastPublish;
-        $this->brandRef = $brandRef;
-        $this->version = $version;
-        $this->enabled = $enabled;
-        $this->privateWidgets = $privateWidgets;
-        $this->mobileSiteRef = $mobileSiteRef;
-        $this->mobile = $mobile;
-        $this->profileRef = $profileRef;
+    public function __construct(
+        public int $ref,
+        public array $domains,
+        public ?int $contentMapSite,
+        public ?int $template,
+        public Domain $primaryDomain,
+        public ?array $lastPublish,
+        public int $brandRef,
+        public int $version,
+        public bool $enabled,
+        public ?bool $privateWidgets,
+        public ?int $mobileSiteRef,
+        public bool $mobile,
+        public ?int $profileRef,
+    ) {
     }
 
     /**
@@ -102,19 +61,19 @@ final class Site implements DomainObjectInterface
         }
 
         return new Site(
-            $json['ref'],
-            $domains,
-            $json['contentMapSite'] ?? null,
-            $json['template'] ?? null,
-            Domain::fromArray($json['primaryDomain']),
-            $json['lastPublish'],
-            $json['brandRef'],
-            $json['version'],
-            $json['enabled'],
-            is_numeric($json['privateWidgets']) ? (bool) $json['privateWidgets'] : null,
-            $json['mobileSiteRef'] ?? null,
-            $json['mobile'],
-            $json['profileRef'] ?? null,
+            ref: $json['ref'],
+            domains: $domains,
+            contentMapSite: $json['contentMapSite'] ?? null,
+            template: $json['template'] ?? null,
+            primaryDomain: Domain::fromArray($json['primaryDomain']),
+            lastPublish: $json['lastPublish'],
+            brandRef: $json['brandRef'],
+            version: $json['version'],
+            enabled: $json['enabled'],
+            privateWidgets: is_numeric($json['privateWidgets']) ? (bool) $json['privateWidgets'] : null,
+            mobileSiteRef: $json['mobileSiteRef'] ?? null,
+            mobile: $json['mobile'],
+            profileRef: $json['profileRef'] ?? null,
         );
     }
 }
