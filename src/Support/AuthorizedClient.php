d62ba34af921b768b382de3482a9a6a78fbf9e02
Daan Dorchholz <daan.dorcholz@yourhosting.nl>
2025-03-12 16:56:09 +0100

    remove private log method

--- src/Support/AuthorizedClient.php
+++ src/Support/AuthorizedClient.php
@@ -28,13 +28,17 @@ final class AuthorizedClient
      * @param array<mixed>         $guzzleOptions
      * @param LoggerInterface|null $logger
      */
-    public function __construct(string $baseUrl, string $username, string $password, array $guzzleOptions = [], ?LoggerInterface $logger = null)
-    {
+    public function __construct(
+        string $baseUrl,
+        string $username,
+        string $password,
+        array $guzzleOptions = [],
+        ?LoggerInterface $logger = null
+    ) {
         $this->logger = $logger;
-
         $this->client = new Client(array_merge($guzzleOptions, [
             'base_uri' => $baseUrl,
-            'auth' => [$username, $password],
+            'auth'     => [$username, $password],
         ]));
     }
 
@@ -110,7 +114,7 @@ final class AuthorizedClient
     {
         // Build request options.
         $metaData = [
-            'headers' => [],
+            'headers'     => [],
             'http_errors' => false,
         ];
 
@@ -119,43 +123,31 @@ final class AuthorizedClient
         }
 
         // Log request.
+        $fullEndpoint = $endpoint . $this->buildQuery($query);
         $logContext = [
-            'meta_data' => $metaData,
-            'method' => $method,
-            'endpoint' => $endpoint . $this->buildQuery($query),
-            'body' => $body,
+            'meta_data'         => $metaData,
+            'method'            => $method,
+            'endpoint'          => $fullEndpoint,
+            'body'              => $body,
             'expected_response' => $expectedResponse ?? 'NOT SET',
         ];
-        $this->log(
-            sprintf(
-                'BaseKit.REQUEST: %s %s',
-                strtoupper($method),
-                $endpoint . $this->buildQuery($query)
-            ),
-            $logContext
-        );
+        $this->logger?->debug(sprintf('BaseKit.REQUEST: %s %s', strtoupper($method), $fullEndpoint), $logContext);
 
         // Send request.
         try {
-            $response = $this->client->request($method, $endpoint . $this->buildQuery($query), $metaData);
+            $response = $this->client->request($method, $fullEndpoint, $metaData);
         } catch (GuzzleException $exception) {
-            $this->log(
-                sprintf('BaseKit.EXCEPTION: %s', $exception->getMessage()),
-                ['exception' => $exception]
-            );
+            $this->logger?->debug(sprintf('BaseKit.EXCEPTION: %s', $exception->getMessage()), ['exception' => $exception]);
             throw new BaseKitRequestException('Error while sending request to BaseKit', $exception->getCode(), $exception);
         }
+
         return $this->handleResponse($response, $expectedResponse);
     }
 
     private function handleResponse(ResponseInterface $response, ?int $expectedResponse = null): BaseKitResponse
     {
-        $this->log(
-            sprintf(
-                'BaseKit.RESPONSE: %s - BODY: %s',
-                $response->getStatusCode(),
-                (string) $response->getBody()
-            ),
+        $this->logger?->debug(
+            sprintf('BaseKit.RESPONSE: %s - BODY: %s', $response->getStatusCode(), (string) $response->getBody()),
             [
                 'response_code'     => $response->getStatusCode(),
                 'response_body'     => (string) $response->getBody(),
@@ -176,7 +168,9 @@ final class AuthorizedClient
             case $response->getStatusCode() === 404:
                 throw new BadRequestException('Not Found: ' . $response->getBody());
             default:
-                throw new BaseKitRequestException("Unexpected response: (got {$response->getStatusCode()}, expected {$expectedResponse}). Body: " . $response->getBody());
+                throw new BaseKitRequestException(
+                    "Unexpected response: (got {$response->getStatusCode()}, expected {$expectedResponse}). Body: " . $response->getBody()
+                );
         }
     }
 
@@ -196,16 +190,5 @@ final class AuthorizedClient
             return $response->getStatusCode() === $expectedResponse;
         }
         return $response->getStatusCode() >= 200 && $response->getStatusCode() < 300;
-    }
-
-    /**
-     * @param string       $message
-     * @param array<mixed> $context
-     */
-    private function log(string $message, array $context = []): void
-    {
-        if ($this->logger instanceof LoggerInterface) {
-            $this->logger->debug($message, $context);
-        }
     }
 }
